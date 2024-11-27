337e7004155d36d2c7e3cb45e341945da921c21d
mrtindanzor <ktindanzor@gmail.com>
2024-11-27 23:13:24 +0000

    remove points notes

--- assets/routes/all/verify.js
+++ assets/routes/all/verify.js
@@ -3,8 +3,7 @@ import { Router } from "express"
 
 const verifyroute = Router(),
       certDb = [{"name":"Thomas Sichone","studentNumber":"608457/10/1","certificateCode":"gism-24-049","studentProgramme":"Doctor of Business Administration","dateCompleted":"4th July, 2024","_id":"1"},
-        {"name":"Jacob Sichone","studentNumber":"608456/10/1","certificateCode":"gism-24-085","studentProgramme":"Doctor of Business Administration","dateCompleted":"4th July, 2024","_id":"2"},
-        {"name":"simon Sichone","studentNumber":"608456/10/1","certificateCode":"gism-gcbt-24-085","studentProgramme":"Doctor of Business Administration","dateCompleted":"4th July, 2024","_id":"2"}
+        {"name":"Jacob Sichone","studentNumber":"608456/10/1","certificateCode":"gism-24-085","studentProgramme":"Doctor of Business Administration","dateCompleted":"4th July, 2024","_id":"2"}
         ]
 
 
