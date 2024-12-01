9d9a896e383b93cc7c3e0fb52f1fef8724cf7f4e
mrtindanzor <ktindanzor@gmail.com>
2024-12-01 23:17:38 +0000

    Update verify.js

--- assets/public/scripts/verify.js
+++ assets/public/scripts/verify.js
@@ -8,27 +8,25 @@ verifyForm.addEventListener('submit', (e) => {
   let certificateNum = certificateNumber.value
   if(certificateNum.length < 1) return
   fetch(`/verify/${certificateNum}`)
-        .then((res) => res.json())
-        .then(data => {
-          if(data.status === 500) {
-            showFoundStudent.innerHTML =  `<b>${data.msg}</b>`
-        }
-          if(data.status === 404) {
-            showFoundStudent.innerHTML =  `<b class="invalid-code">${data.msg}</b>`
-        }
-          if(data.status === 200) {
-            let name = data.user.name
-            let programme = data.user.programme
-            showFoundStudent.innerHTML =  `
-        <div class="student-details">
-             <div><b for="student-name">Name: </b><span class="student-name">${name.toUpperCase()}</span></div>
-            <div><b for="student-number">Student number: </b><span class="student-number">${data.user.studentNumber}</span></div>
-            <div><b for="student-certificate-num">Certificate code: </b><span class="student-certificate-num">${data.user.certificateCode.toUpperCase()}</span></div>
-            <div><b for="verification-status">Status: </b><span class="verification-status">VALID</span></div>
-            <div><b for="student-course">Programme: </b><span class="student-course">${programme.toUpperCase()}</span></div>
-            <div><b for="date-completed">Date completed: </b><span class="date-completed">${data.user.dateCompleted.toUpperCase()}</span></div>
-        </div>
-          `
-        }
-        })
+  .then((res) => res.json())
+  .then(data => {
+  if(data.status === 500) {
+    return showFoundStudent.innerHTML =  `<b>${data.msg}</b>`
+  }
+  if(data.status === 404) {
+    return showFoundStudent.innerHTML =  `<b class="invalid-code">${data.msg}</b>`
+  }
+  let name = data.user.name
+  let programme = data.user.programme
+  showFoundStudent.innerHTML =  `
+    <div class="student-details">
+        <div><b for="student-name">Name: </b><span class="student-name">${name.toUpperCase()}</span></div>
+      <div><b for="student-number">Student number: </b><span class="student-number">${data.user.studentNumber}</span></div>
+      <div><b for="student-certificate-num">Certificate code: </b><span class="student-certificate-num">${data.user.certificateCode.toUpperCase()}</span></div>
+      <div><b for="verification-status">Status: </b><span class="verification-status">VALID</span></div>
+      <div><b for="student-course">Programme: </b><span class="student-course">${programme.toUpperCase()}</span></div>
+      <div><b for="date-completed">Date completed: </b><span class="date-completed">${data.user.dateCompleted.toUpperCase()}</span></div>
+    </div>
+`
+  })
 })
