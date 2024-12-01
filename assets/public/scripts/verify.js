51f932c0021bba60460760b134e93045de14b8da
mrtindanzor <ktindanzor@gmail.com>
2024-12-01 22:19:43 +0000

    Update verify.js

--- assets/public/scripts/verify.js
+++ assets/public/scripts/verify.js
@@ -17,7 +17,17 @@ verifyForm.addEventListener('submit', (e) => {
             showFoundStudent.innerHTML =  `<b class="invalid-code">${data.msg}</b>`
         }
           if(data.status === 200) {
-            
+            let name = data.user.name
+            showFoundStudent.innerHTML =  `
+        <div class="student-details">
+             <div><b for="student-name">Name: </b><span class="student-name">${name.toUpperCase()}</span></div>
+            <div><b for="student-number">Student number: </b><span class="student-number">${data.user.studentNumber}</span></div>
+            <div><b for="student-certificate-num">Certificate code: </b><span class="student-certificate-num">${data.user.certificateCode.toUpperCase()}</span></div>
+            <div><b for="verification-status">Status: </b><span class="verification-status">VALID</span></div>
+            <div><b for="student-course">Programme: </b><span class="student-course"></span></div>
+            <div><b for="date-completed">Date completed: </b><span class="date-completed">${data.user.dateCompleted.toUpperCase()}</span></div>
+        </div>
+          `
         }
         })
 })
