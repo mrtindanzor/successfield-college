28be5849fbe209c2392803844a95094cad0336a7
mrtindanzor <ktindanzor@gmail.com>
2024-12-04 21:03:41 +0000

    edited header

--- assets/public/scripts/common.js
+++ assets/public/scripts/common.js
@@ -1,36 +1,30 @@
-const cartButton = document.querySelector('#cart-button-checkbox'),
-      cartForm = document.querySelector('.cart-form'),
-      closeCart = document.querySelector('.cart-close'),
-      courseButton = document.querySelector('.menu-item span.button'),
+const courseListBtn = document.querySelector('.course-list-button'),
       menuBtn = document.querySelector('.menu-button'),
-      svgs = document.querySelector('svg')
+      svgs = document.querySelector('svg'),hideShow = async (object, display= 'block') => {
+        const currentDisplay = await window.getComputedStyle(object).display;
+        
+        if(currentDisplay === 'none'){
+            object.style.display = display   
+          object.style.flexDirection = 'column'
+      } else {
+          object.style.display = 'none'
+        }
+      },
+      menuList = document.querySelector('ul.menu-list'),
+      courseList = document.querySelector('.course-list-menu')
 
 svgs.style.opacity = 1
-if(courseButton != null){
-  courseButton.addEventListener('click', ()=> {
-    document.querySelector('.courses-categories').classList.toggle('active')
-  })
-}
 
-if(menuBtn != null) menuBtn.addEventListener('click', () => document.querySelector('.menu').classList.toggle('active'))
-
-document.body.addEventListener('click', (e)=>{
-  if(menuBtn != null) if(!e.target.classList.contains('button') && !e.target.classList.contains('course-option') && !e.target.classList.contains('course-categories')) document.querySelector('.courses-categories').classList.remove('active')
+menuBtn.addEventListener('click', () => {
+  hideShow(menuList, 'flex')
+})
+courseListBtn.addEventListener('click', () => {
+  hideShow(courseList)
+})
+document.addEventListener('click', (e) => {
+  if(!menuBtn.contains(e.target) && !menuList.contains(e.target)) {
+    courseList.style.display = 'none'
+    menuList.style.display = 'none'
     
-    if(menuBtn != null) if(!e.target.classList.contains('menu-button') && !e.target.classList.contains('menu-button-icon') && !e.target.classList.contains('menu-item') && !e.target.parentElement.classList.contains('menu-item')) document.querySelector('.menu').classList.remove('active')
-  })
-
-// if(cartButton != null){
-//   document.body.addEventListener('click', (e) => {
-//   if(!e.target.classList.contains('cart-button') && !e.target.classList.contains('cart-button-icon') && !e.target.classList.contains('cart-close') && !e.target.classList.contains('cart-checkout') && !e.target.classList.contains('cart-list'))
-//     if(cartForm.classList.contains('active')){
-//       cartForm.classList.remove('active')
-//     }
-//   })
-// } 
- 
-if(cartButton != null) cartButton.addEventListener('click', () => cartForm.classList.toggle('active'))
-if(closeCart != null) closeCart.addEventListener('click', (event) => {
-  event.preventDefault()
-  cartForm.classList.remove('active')
+  }
 })
