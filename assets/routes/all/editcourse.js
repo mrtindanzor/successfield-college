caee988e65d14d20b02f863d8712244668d110bc
mrtindanzor <ktindanzor@gmail.com>
2024-12-07 02:55:14 +0000

    search with id instead of course when updating

--- assets/routes/all/editcourse.js
+++ assets/routes/all/editcourse.js
@@ -21,9 +21,10 @@ editcourseRoute.post('/findcourse', checkAdmin, async (req, res) => {
 
 editcourseRoute.post('/editcourse', checkAdmin, async (req, res) => {
   const newCourse = req.body,
-    courseName = newCourse.course
+    courseName = newCourse.course,
+    id = newCourse.id
   if(!newCourse) return res.status(400).json({status: 400, msg: 'No course was sent'})
-  const isCourse = await courseModel.findOne({course: courseName})
+  const isCourse = await courseModel.findOne({_id: id})
   if(!isCourse) return res.status(404).json({status: 404, msg: 'No course found'})
   if(isCourse._id.toString() !== newCourse.id) return res.status(404).json({status: 404, msg: 'Course id not matching'})
 
