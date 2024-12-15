90e54516d5025de32aef17b54b6c94a797ff6eba
mrtindanzor <ktindanzor@gmail.com>
2024-12-15 22:29:28 +0000

    added copyright

--- assets/public/scripts/common.js
+++ assets/public/scripts/common.js
@@ -1,23 +1,30 @@
 const courseListBtn = document.querySelector('.course-list-button'),
-      menuBtn = document.querySelector('.menu-button'),
-      svgs = document.querySelector('svg'),hideShow = async (object, display= 'block') => {
-        const currentDisplay = await window.getComputedStyle(object).display;
-        
-        if(currentDisplay === 'none'){
-            object.style.display = display   
-          object.style.flexDirection = 'column'
-      } else {
-          object.style.display = 'none'
-        }
-      },
-      menuList = document.querySelector('ul.menu-list'),
-      courseList = document.querySelector('.course-list-menu')
+  menuBtn = document.querySelector('.menu-button'),
+  dateContainer = document.querySelector('.date-year'),
+  currentYear = new Date().getFullYear(),
+  svgs = document.querySelectorAll('svg'),
+  hideShow = (object, display= 'block') => {
+    const currentDisplay = window.getComputedStyle(object).display;
+    
+    if(currentDisplay === 'none'){
+        object.style.display = display   
+      object.style.flexDirection = 'column'
+  } else {
+      object.style.display = 'none'
+    }
+  },
+  menuList = document.querySelector('ul.menu-list'),
+  courseList = document.querySelector('.course-list-menu')
 
-svgs.style.opacity = 1
+svgs.forEach(el => {
+  el.style.opacity = 1
+})
+
 
 menuBtn.addEventListener('click', () => {
   hideShow(menuList, 'flex')
 })
+dateContainer.textContent = currentYear
 courseListBtn.addEventListener('click', () => {
   hideShow(courseList)
 })
