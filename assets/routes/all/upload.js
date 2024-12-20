041e8fa69cc6311f27adcb6bde53fc923c1c3f1d
mrtindanzor <ktindanzor@gmail.com>
2024-12-20 23:45:28 +0000

    Update upload.js

--- assets/routes/all/upload.js
+++ assets/routes/all/upload.js
@@ -4,19 +4,26 @@ import multer from  "multer"
 import fs from 'fs'
 
 const uploadRoute = Router(),
-  storage = {
-    destination: (res, file, cb) => {
-
+  storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, uploadPath)
     },
-    filename: (res, file, cb) => {
-      
+    filename: (req, file, cb) => {
+      const token = Date.now(),
+      split = file.originalname.split('.'),
+      ext = '.' + split[split.length - 1],
+      name = token+ext
+
+      cb(null, name)
     }
-  }
+  }),
+  upload = multer({ storage })
+  
 uploadRoute.get('/upload', (req, res) => {
   res.render('index', {page: 'upload', title: 'upload images'})
 })
-uploadRoute.put('/upload', (req, res) => {
-  if(!req.body.picture) return res.status(400).json({status: 400, msg: 'No image sent'})
+uploadRoute.put('/upload', upload.single('picture'), (req, res) => {
+  if(!req.picture) return res.status(400).json({status: 400, msg: 'No image sent'})
   return res.status(200).json({status: 400, msg: 'image sent'})
 })
 
