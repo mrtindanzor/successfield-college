4fbbe25ce1163bf40c6c916a577a3611085b65af
mrtindanzor <ktindanzor@gmail.com>
2024-12-22 20:19:38 +0000

    removed test

--- assets/routes/all/test.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-
-const testroute = Router()
-
-testroute.get('/test', (req, res) => {
-  res.render('index', {page: "test", title: 'test'})
-})
-
-export default testroute
