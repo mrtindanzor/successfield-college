fb14d9bdde74c1c373cd055e73e1e5bdc8971c42
mrtindanzor <ktindanzor@gmail.com>
2024-12-22 23:57:16 +0000

    fixedmissing image in slidshow

--- assets/public/scripts/home.js
+++ assets/public/scripts/home.js
@@ -1,5 +1,5 @@
 const slideshowWrapper = document.querySelector('.slideshow-wrapper'),
-imgs = ['000015.jpg', '000016.jpg', '000017.jpg', '000018.jpg', '000019.jpg', '000020.jpg', '000021.jpg', '000022.jpg', '000023.jpg', '000024.jpg', '000025.jpg', '000026.jpg', '000027.jpg', '000028.jpg', '000029.jpg', '000030.jpg', '000031.jpg', '000033.jpg', '000034.jpg',  '000035.jpg', '000036.jpg',  '000037.jpg', '000039.jpg', '000041.jpg', '000045.jpg', '000046.jpg']
+imgs = ['000015.jpg', '000016.jpg', '000017.jpg', '000018.jpg', '000019.jpg', '000020.jpg', '000021.jpg', '000022.jpg', '000023.jpg', '000024.jpg', '000025.jpg', '000026.jpg', '000027.jpg', '000028.jpg', '000029.jpg', '000030.jpg', '000031.jpg', '000032.jpg',  '000033.jpg', '000034.jpg',  '000035.jpg', '000036.jpg',  '000037.jpg', '000039.jpg', '000041.jpg', '000045.jpg']
 
 let i = 1
 imgs.forEach(el => {
