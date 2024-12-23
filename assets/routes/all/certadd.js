4106d4228ddd78764365e44ca5819ce3a086e798
mrtindanzor <ktindanzor@gmail.com>
2024-12-23 01:38:53 +0000

    added mail sending after adding certificate

--- assets/routes/all/certadd.js
+++ assets/routes/all/certadd.js
@@ -1,11 +1,12 @@
 import { Router } from "express"
 import { certificateModel } from "../../../dependencies.js"
-
+import { sendMailAsync } from "./sendmail.js"
+import mailTemplates from "./mailtemplates.js"
+import { env, userModel } from "../../../dependencies.js"
 const certaddRoute = Router()
 
 certaddRoute.put('/certadd', async (req, res) => {
   const certificate = req.body
-  
   const certificateCode = certificate.certificateCode.toLowerCase().trim(),
   user = await certificateModel.findOne({certificateCode})
 
@@ -14,11 +15,24 @@ certaddRoute.put('/certadd', async (req, res) => {
   const newCertificate = new certificateModel(certificate)
 
   newCertificate.save()
-    .then(() => {
+    .then( async () => {
       if(newCertificate.isNew) return res.status(400).json({status: 304, msg: 'could not add file'})
+      const users = await userModel.find({admin: true}).catch(err => console.log(err))
+      if(users){
+        const name = certificate.name || '',
+            studentNumber = certificate.studentNumber || '',
+            certificateCode = certificate.certificateCode || '', 
+            certificateName = certificate.certificate || '',
+            subject = 'New certificate issued to '+name.toUpperCase()
+            users.forEach(async el => {
+              const admin = el.firstname + ' ' + el.surname,
+                html = (new mailTemplates).newCertificateIssued(admin, name, studentNumber, certificateCode, certificateName)
+              await sendMailAsync(subject, html, el.email)
+              })
+            }
+        })
       return res.status(201).json({status: 201, msg: 'added successfully'})
     })
-})
 
 certaddRoute.get('/certadd', (req, res) => {
   res.status(200).render('index', {page: 'certadd', title: 'Add a certificate'})
--- assets/routes/all/mailtemplates.js
+++ assets/routes/all/mailtemplates.js
@@ -76,6 +76,18 @@ class mailTemplates{
     `
   }
   
+  newCertificateIssued = (admin, name, studentNumber, certificateCode, certificate ) => {
+    return `
+     <h3>Mr. ${admin.toUpperCase()},</h3>
+        <br>
+      <p>
+       A new certificate has been is issued to ${name.toUpperCase()} with student number ${studentNumber.toUpperCase()} for ${certificate.toUpperCase()}. The certificate code is ${certificateCode.toUpperCase()}.
+        <br>
+        Adios.
+      </p>
+    `
+  }
+  
   setAdminStatus = (name, email, status) => {
     let color = 'red'
     if(status === true) color = 'green'
