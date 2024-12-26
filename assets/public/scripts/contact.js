ef98907a7e6e5c706fd307a1ed56538ef5a8a835
mrtindanzor <ktindanzor@gmail.com>
2024-12-26 15:35:59 +0000

    edited contact page

--- assets/public/scripts/contact.js
+++ assets/public/scripts/contact.js
@@ -1,9 +1,10 @@
 const formEl = document.querySelector('form.email-form'),
   result = document.querySelector('.result')
 
-formEl.addEventListener('submit', e => {
+formEl.addEventListener('submit', async function(e){
   e.preventDefault()
 
+  loader.classList.add('active')
   const formData = new FormData(formEl),
     jsonData = JSON.stringify(Object.fromEntries(formData)),
     uri = '/contact',
@@ -13,36 +14,35 @@ formEl.addEventListener('submit', e => {
         'Content-Type': 'application/json'
       },
       body: jsonData
-    }
+    },
+    response = await fetch(uri, options),
+    res = await response.json()
+  if(res.status === 201){
+    result.innerHTML = `
+    <div class="mail-sent">
+      <span class="text">
+      ${res.mailIcon}
+        <span>${res.msg}</span>
+      </span>
+      <button>OK</button>
+    </div>
+  `
+  formEl.reset()
+  } 
+  if(res.status !== 201) result.innerHTML = `
+    <div class="mail-not-sent">
+      <span class="text">
+        ${res.mailIcon}
+        <span>${res.msg}</span>
+      </span>
+      <button>OK</button>
+    </div>
+  `
 
-    fetch(uri, options)
-      .then(res => res.json())
-      .then(data => {
-        if(data.status === 201){
-          result.innerHTML = `
-          <div class="mail-sent">
-            <span class="text">
-            ${data.mailIcon}
-              <span>${data.msg}</span>
-            </span>
-            <button>OK</button>
-          </div>
-        `
-        formEl.reset()
-        } 
-        if(data.status !== 201) result.innerHTML = `
-          <div class="mail-not-sent">
-            <span class="text">
-              ${data.mailIcon}
-              <span>${data.msg}</span>
-            </span>
-            <button>OK</button>
-          </div>
-        `
-
-        result.classList.add('active')
-        const closeBtn = document.querySelector('.result button')
-        closeBtn.addEventListener('click', () => result.classList.remove('active'))
-
-      })
+  result.classList.add('active')
+  const closeBtn = document.querySelector('.result button')
+  closeBtn.addEventListener('click', function(){
+    result.classList.remove('active')
+    loader.classList.remove('active')
+  })
 })
