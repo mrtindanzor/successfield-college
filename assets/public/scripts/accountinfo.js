b7bbf2d12e8d8b7859fc5a1140c6d03549307388
mrtindanzor <ktindanzor@gmail.com>
2024-12-31 00:12:16 +0000

    added active to loader in accout informaition

--- assets/public/scripts/accountinfo.js
+++ assets/public/scripts/accountinfo.js
@@ -2,4 +2,6 @@ const formEl = document.querySelector('.page-wrapper form.form-container')
 
 formEl.addEventListener('submit', function(e){
   e.preventDefault()
+
+  loader.classList.add('active')
 })
