1207f81c2602e3bb1575e2838d4ec7f1beb9bd0c
mrtindanzor <ktindanzor@gmail.com>
2025-01-03 22:49:20 +0000

    verify email

--- assets/public/scripts/verifyemail.js
+++ assets/public/scripts/verifyemail.js
@@ -2,22 +2,18 @@ const timeout = document.querySelector('.timeout'),
   inputEl = document.querySelector('.email'),
   resendBtn = document.querySelector('.resend-button'),
   result = document.querySelector('.result')
-let count = 10
+
 function counter(object){
+  let count = 10
+  object.textContent = count
   setInterval(()=> {
     if(count === 1) return
     count--
     object.textContent = count
   }, 1000)
 }
-function redirect(){
-  setTimeout(() => {
-    window.location.href = '/users/login'
-  }, 5000);
-}
-
+function redirect(){ setTimeout(() => { window.location.href = '/users/login' }, 5000) }
 if(timeout){
-  timeout.textContent = count
   counter(timeout)
   redirect()
 }
@@ -37,14 +33,14 @@ if(resendBtn){
      response = await fetch(uri, options),
      res = await response.json()
     if(res.status === 302) {
-      result.innerHTML = `<div class="email-not-sent">${res.msg}, redirecting in<span class="timeout">${count}</span>secs</div>`
+      result.innerHTML = `<span class="success">${res.msg}, redirecting in<span class="timeout"></span>secs</span>`
       const timeout = document.querySelector('.timeout')
       counter(timeout)
       redirect()
       return 
     }
-    if(res.status === 200) result.innerHTML = `<div class="email-sent">${res.msg}</div>`
-    if(res.status !== 200) result.innerHTML = `<div class="email-not-sent">${res.msg}</div>`
+    if(res.status === 200) result.innerHTML = `<span class="success">${res.msg}</span>`
+    if(res.status !== 200) result.innerHTML = `<span class="failed">${res.msg}</span>`
     resetElHtml(result)
   })
 }
