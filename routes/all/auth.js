2772bacf77ee8b271ac0601bfea117d0e155c659
Mr. Tindanzor <63526717+mrtindanzor@users.noreply.github.com>
2025-02-22 21:08:02 +0000

    Reactivated server for registration

--- routes/all/auth.js
+++ routes/all/auth.js
@@ -64,8 +64,6 @@ authroute.patch('/', async function(req, res){
 })
 authroute.post('/join', authenticated,  async function(req, res){
 	
-	return res.json({status: 400, msg: 'Server inactive, try again later'})
-	
   let {firstname, middlename, surname, email, password, cpassword } = req.body
 
   if(!firstname)  return res.status(400).json({status: 400, msg: 'Add your firstname'})
