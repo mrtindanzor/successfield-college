94c5b8e11a4f6ac978f11362c7f08bdf78e82621
Mr. Tindanzor <63526717+mrtindanzor@users.noreply.github.com>
2025-02-23 16:42:33 +0000

    activated registration

--- routes/all/auth.js
+++ routes/all/auth.js
@@ -63,10 +63,7 @@ authroute.patch('/', async function(req, res){
   return res.status(200).json(users)
 })
 authroute.post('/join', authenticated,  async function(req, res){
-	
-	return res.json({status: 400, msg: 'Server inactive, try again later'})
-	
-  let {firstname, middlename, surname, email, password, cpassword } = req.body
+ let {firstname, middlename, surname, email, password, cpassword } = req.body
 
   if(!firstname)  return res.status(400).json({status: 400, msg: 'Add your firstname'})
   if(!surname)  return res.status(400).json({status: 400, msg: 'Add your surname'}) 
